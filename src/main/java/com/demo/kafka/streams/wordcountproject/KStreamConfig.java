547101f9b8fda47eb830a8239b0982e080fccfab
a122epez <a_komal.rani@airtel.com>
2023-01-10 10:49:14 +0530

    word count window

--- src/main/java/com/demo/kafka/streams/wordcountproject/KStreamConfig.java
+++ src/main/java/com/demo/kafka/streams/wordcountproject/KStreamConfig.java
@@ -14,8 +14,9 @@ public class KStreamConfig {
     @Bean
     public Properties getKStreamProperties() {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-app");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-app-window");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "10.56.76.55:9092");
+        //props.put(StreamsConfig.STATE_DIR_CONFIG, String.format("%s%s", "word-count-application", 8080));
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
--- src/main/java/com/demo/kafka/streams/wordcountproject/WordCountKStream.java
+++ src/main/java/com/demo/kafka/streams/wordcountproject/WordCountKStream.java
@@ -1,14 +1,23 @@
 package com.demo.kafka.streams.wordcountproject;
 
 import jakarta.annotation.PostConstruct;
+import java.time.Duration;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Named;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.Suppressed;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.Stores;
+import org.apache.kafka.streams.state.WindowStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,16 +32,43 @@ public class WordCountKStream {
     @PostConstruct
     public void wordCountStreamsBuilder() {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
-        KStream<String, String> wordCountInput = streamsBuilder.stream("word-count-input-topic");
-        KTable<String, Long> result = wordCountInput.mapValues(value -> value.toLowerCase())
-                .flatMapValues(values -> Arrays.asList(values.split(" ")))
-                .selectKey((x, y) -> y)
-                .groupByKey()
-                .count(Named.as("COUNTS"));
+//        KStream<String, String> wordCountInput = streamsBuilder.stream("word-count-input-topic");
+//        KTable<String, Long> result = wordCountInput.peek((k,v) -> System.out.println(k + " " + v))
+//                 .mapValues(value -> value.toLowerCase())
+//                .flatMapValues(values -> Arrays.asList(values.split(" ")))
+//                .selectKey((x, y) -> y)
+//                .groupByKey()
+//                .count(Named.as("COUNTS"));
 
-        result.toStream().to("word-count-output-topic", Produced.with(Serdes.String(), Serdes.Long()));
+                KStream<String, String> wordCountInput = streamsBuilder.stream("word-count-input-topic");
+        wordCountInput.peek((k,v) -> System.out.println(k + " " + v))
+            .mapValues(value -> value.toLowerCase())
+            .flatMapValues(values -> Arrays.asList(values.split(" ")))
+            .selectKey((x, y) -> "567")
+            .groupByKey()
+            .windowedBy(TimeWindows.ofSizeAndGrace(
+                Duration.ofMinutes(1), Duration.ZERO))
+            .count(materializedAsWindowStore("test", Serdes.String(), Serdes.Long()))
+            .suppress(Suppressed.untilWindowCloses(Suppressed.BufferConfig.unbounded())
+                .withName("test"))
+            .toStream()
+            .foreach((k,v) -> System.out.println("Windowed Result : " + k.window().start() + " " + System.currentTimeMillis() + " " + v));
+
+        //result.toStream().to("word-count-output-topic", Produced.with(Serdes.String(), Serdes.Long()));
+
         Topology topology = streamsBuilder.build();
         KafkaStreams kafkaStreams = new KafkaStreams(topology, kStreamConfig.getKStreamProperties());
         kafkaStreams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+    }
+
+    private <K, V> Materialized<K, V, WindowStore<Bytes, byte[]>> materializedAsWindowStore(
+        String storeName, Serde<K> keySerde, Serde<V> valueSerde) {
+        return Materialized.<K, V>as(Stores.persistentWindowStore(storeName,
+                Duration.ofMinutes(1),
+                Duration.ofMinutes(1), true))
+            .withKeySerde(keySerde).withValueSerde(valueSerde)
+            .withLoggingDisabled()
+            .withRetention(Duration.ofMinutes(10));
     }
 }
